fca9f405618d4db10d94d0ac7e69511dfc1ff19d
Andrew Curd <andrewc2695@gmail.com>
2020-12-04 12:06:47 -0800

    intialized Polytree node

--- w3d5/skeleton/lib/00_tree_node.rb
+++ w3d5/skeleton/lib/00_tree_node.rb
@@ -1,3 +1,26 @@
 class PolyTreeNode
 
+    def initialize(value)
+        @parent = nil
+        @children = []
+        @value = value
+    end
+
+    def parent
+        @parent
+    end
+
+    def children
+        @children
+    end
+
+    def value
+        #value at node
+    end
+
+    def parent=(node_instance)
+        @parent = node_instance
+        node_instance.children << self
+    end
+
 end
