f3231d5a0203946a09eddc1957b9ba20042ec133
Justin Cheasty <cheasty@gmail.com>
2020-12-04 16:41:51 -0800

    Created root_node method

--- w3d5/Knights Travails/knights_travails.rb
+++ w3d5/Knights Travails/knights_travails.rb
@@ -1,11 +1,31 @@
+require_relative "../skeleton/lib/00_tree_node.rb"
+
 class Knight
-    def intialize(starting_pos)
+    def self.root_node
+        starting_node = PolyTreeNode.new(@starting_pos) #might need "@"
+    end
+    #class methond self.valid_moves
+    def initialize(starting_pos)
         @knight = knight
         @starting_pos = starting_pos
+        @considered_pos = [starting_pos]
+        Knight.root_node
     end
 
     def knight
         @knight
     end
 
+    def starting_pos
+        @starting_pos
+    end
+
+    def new_move_positions(pos)
+
+    end
+
+
 end
+
+
+# k = Knight.new([0,0])
