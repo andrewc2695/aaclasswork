f367fd0f5e51745ba8f8d9c3d95531739b553092
numbercdli <jhwangp@gmail.com>
2020-12-08 17:33:57 -0500

    class inher take 2.5

--- w4d2/class_inheritence.rb
+++ w4d2/class_inheritence.rb
@@ -1,35 +1,61 @@
-
+ 
 class Employee
+    attr_reader :salary
     def initialize(name, title, salary, boss)
         @name = name
         @title = title
         @salary = salary
         @boss = boss
     end
-
+ 
     def bonus(multiplier)
         return bonus = @salary * multiplier
     end
 end
-
+ 
 class Manager < Employee
-   def initialize(name, title, salary, boss)
+    attr_reader :employees
+    
+    def initialize(name, title, salary, boss)
         @name = name
         @title = title
         @salary = salary
         @boss = boss
         @employees = []
     end
-
+ 
     def new_employee(new_worker)
         @employees << new_worker
     end
-
+ 
     def bonus(multiplier)
-        return bonus = @employees.flatten.sum * multiplier
+        return multiplier * subordinate_salary
+    end
+ 
+    def subordinate_salary
+        sub_salary = 0
+ 
+        @employees.each do |worker|
+            if worker.is_a?(Manager)
+                sub_salary += worker.subordinate_salary
+            else
+                sub_salary += worker.salary
+            end
+        end
+        sub_salary
     end
 end
-
+ 
 ned = Manager.new('Ned', 'Founder', 1000000, nil)
 darren = Manager.new('Darren', 'TA Manager', 78000, ned)
 shawna = Employee.new('Shawna', 'TA', 12000, darren)
+david = Employee.new('David', 'TA', 10000, darren)
+ 
+ned.new_employee(darren)
+darren.new_employee(shawna)
+darren.new_employee(david)
+ 
+p ned.bonus(5) # => 500_000
+p darren.bonus(4) # => 88_000
+p david.bonus(3) # => 30_000
+
