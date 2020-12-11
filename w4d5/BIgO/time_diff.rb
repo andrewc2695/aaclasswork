b5f3fb3373c451f645933b11f7fe7ff735837857
Edwin <edwinzhou99@gmail.com>
2020-12-11 11:43:28 -0800

    optimize time for functions

--- w4d5/BIgO/time_diff.rb
+++ w4d5/BIgO/time_diff.rb
@@ -24,14 +24,39 @@ end
 
 def largest_contiguous_subsum(array)
     subs = []
-    (0...array.length - 1).each do |i|
-        (i..array.length - 1).each do |i2|
-            subs << array[i..i2]
+    (0...array.length - 1).each do |i|#n
+        (i..array.length - 1).each do |i2|#n
+            subs << array[i..i2] #n
         end
     end #(n^2)
-    subs.map {|sub| sub.inject(:+)}.sort[-1] #(3n?)
+    subs.map {|sub| sub.inject(:+)}.sort[-1] #(nlogn)
 end
 
 # list = [2, 3, -6, 7, -6, 7]
 # p largest_contiguous_subsum(list)
 #O(n^3) or O(n^2)
+
+
+def largest_contiguous_subsum_2(arr)
+    largest_sum = arr.first
+    largest_index = 0
+    arr.each_with_index do |el,i| #O(n)
+        if el > largest_sum
+            largest_sum = el 
+            largest_index = i
+        end
+    end
+    new_arr = arr[0...largest_index] + arr[largest_index+1..-1] #O(2n)or#O(n)
+   
+    new_arr.each do |el| #O(n)
+        temp_sum = el + largest_sum
+        largest_sum = temp_sum if temp_sum > largest_sum
+    end
+    largest_sum
+end #O(n)
+
+
+ list = [-5, -1, -3]
+p largest_contiguous_subsum_2(list) # => 8
+
+
