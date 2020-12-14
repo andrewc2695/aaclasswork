42947ca6a2ba8acee94b55bf511aa138953d6447
Joshua Clarence D. Sadsad <joshsad2@gmail.com>
2020-12-14 14:15:38 -0800

    finished hashing #string

--- w5d1/my_hash_map/lib/p02_hashing.rb
+++ w5d1/my_hash_map/lib/p02_hashing.rb
@@ -1,14 +1,23 @@
 class Integer
   # Integer#hash already implemented for you
 end
-
+# index = num
+# [1,2,3] => 011223
 class Array
   def hash
   end
 end
-
+# convert every letter to its alphabet index.
+# cat = [3, 1, 20].join => "3120"
 class String
   def hash
+    alph = ("a".."z").to_a
+    num_str = ""
+    self.each_char do |char|
+      index = alph.index(char)
+      num_str += index.to_s
+    end
+    num_str.to_i.hash
   end
 end
 
