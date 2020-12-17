380e2384396db0fcdfacba219d444addc9a5157a
Andrew Curd <andrewc2695@gmail.com>
2020-12-17 14:31:29 -0800

    started associations

--- w5d4/rails w5d4/app/models/course.rb
+++ w5d4/rails w5d4/app/models/course.rb
@@ -1,2 +1,16 @@
 class Course < ApplicationRecord
+    belongs_to :enrollment.
+        primary_ley: :id,
+        foreign_key: :prereq_id,
+        class_name: :Enrollment
+
+    belongs_to :instructor,
+        primary_ley: :id,
+        foreign_key: :instructor_id,
+        class_name: :User
+
+     belongs_to :student,
+        primary_ley: :id,
+        foreign_key: :student_id,
+        class_name: :User
 end
--- w5d4/rails w5d4/app/models/enrollment.rb
+++ w5d4/rails w5d4/app/models/enrollment.rb
@@ -1,2 +1,11 @@
 class Enrollment < ApplicationRecord
+    has_many :courses,
+        primary_key: :id,
+        foreign_key: :prereq_id,
+        class_name: :Course
+
+    hash_many :students,
+        primary_key: :id,
+        foreign_key: :student_id,
+        class_name: :User
 end
