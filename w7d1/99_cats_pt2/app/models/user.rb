c4c3db7ceaf582bd8aba2d6e1c0aabc058d137d7
Trevor Smith <trevor.smith1667@gmail.com>
2021-01-04 14:16:48 -0800

    added sessions controller

--- w7d1/99_cats_pt2/app/models/user.rb
+++ w7d1/99_cats_pt2/app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
     validates :session_token, presence: true, uniqueness: true
     after_initialize :ensure_session_token
 
-    def reset_session_token
+    def reset_session_token!
         self.session_token = SecureRandom::urlsafe_base64
         self.save!
         self.session_token
