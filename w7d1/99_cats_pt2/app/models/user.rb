035883e8f57e3ac2a7d08197e4e5c74e04ff353d
Andrew Curd <andrewc2695@gmail.com>
2021-01-04 13:57:39 -0800

    wrote user class added auth methods

---
+++ w7d1/99_cats_pt2/app/models/user.rb
@@ -0,0 +1,29 @@
+class User < ApplicationRecord
+    validates :username, :password_digest, presence: true
+    validates :session_token, presence: true, uniqueness: true
+    after_initialize :ensure_session_token
+
+    def reset_session_token
+        self.session_token = SecureRandom::urlsafe_base64
+        self.save!
+        self.session_token
+    end
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
+
+    def is_password?(pw)
+        BCrypt::Password.new(self.password_digest).is_password?(pw)
+    end
+
+    def find_by_credentials(username, pw)
+        user = User.find_by(username: username)
+        if user && user.is_password?(pw)
+            user
+        else
+            nil
+        end
+    end
+end
