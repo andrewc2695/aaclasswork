0adb0deacc46cc5b22ee1bf7ccc4a114cd889e2f
James Koenig <Koenig.James@Live.com>
2021-01-13 14:17:56 -0800

    worked through transpose function

--- w8d3/js w8d3/phase_1_arrays.js
+++ w8d3/js w8d3/phase_1_arrays.js
@@ -13,7 +13,18 @@ Array.prototype.uniq = function() {
 // arr = [1,1,2,2,3,3,4,5]
 // console.log(arr.uniq())
 
-Array.prototype.transpose = () => {
-    let res = Array(this[0].length);
-    res.map
+Array.prototype.transpose = function() {
+    let res = Array()
+    for(i = 0; i < this[0].length; i++) {
+        res.push(Array());
+        for(j = 0; j < this.length; j++) {
+            res[i].push(this[j][i]);
+        }
+    }
+    return res;
 }
+
+let mtx = [[1,1,2],[2,3,3],[4,5,6]];
+
+console.log(mtx.transpose());
+
