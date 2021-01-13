60ea966ef6cdd0253a5fb05d9f2cb70372e32ff3
James Koenig <Koenig.James@Live.com>
2021-01-13 15:32:01 -0800

    working through my* iterator methods

--- w8d3/js w8d3/phase_2_enumerable.js
+++ w8d3/js w8d3/phase_2_enumerable.js
@@ -17,3 +17,24 @@ arr = [1,2,3];
 // console.log(arr.myMap(function(x) {
 //     return 2 * x;
 // }));
+
+Array.prototype.myReduceNoInitial = function(callback) {
+    var accum;
+    this.myEach(function(ele) {
+        accum = accum ? callback(accum, ele) : ele;
+    });
+    return accum;
+}
+
+Array.prototype.myReduce = function(callback, initialValue) {
+    // var accum;
+    // this.myEach(function(ele) {
+    //     accum = initialValue ? callback()
+    // });
+    // return accum;
+    // if (initialValue === undefined) {
+    //     initialValue = this[0];
+    // } else {
+
+    // }
+}
