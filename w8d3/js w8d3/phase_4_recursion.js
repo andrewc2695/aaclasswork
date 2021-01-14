7bad369bda3b4822612a93e3ade85737f15247ff
Andrew Curd <andrewc2695@gmail.com>
2021-01-13 16:55:16 -0800

    wrote sumRec

--- w8d3/js w8d3/phase_4_recursion.js
+++ w8d3/js w8d3/phase_4_recursion.js
@@ -9,3 +9,12 @@ function range(start,end) {
 
 //console.log(range(1,10));
 
+
+function sumRec(num_arr){
+  let first, rest;
+  [first, ...rest] = num_arr;
+  return first !== undefined ?  first + sumRec(rest) : 0;
+}
+
+console.log(sumRec(range(1, 10)));
+
