da33718a3b35baf157858bebd2b50c9d4a78d115
Andrew Curd <andrewc2695@gmail.com>
2021-01-15 15:14:04 -0800

    wrote absurd bubblesort and myBind

--- w8d5/bubblesort.js
+++ w8d5/bubblesort.js
@@ -6,19 +6,15 @@ const reader = readline.createInterface({
 });
 
 
-function absurdBubbleSort(){
-
-}
-
 function askIfGreaterThan(el1, el2, callback) {
     reader.question(`Is ${el1} greater than ${el2}? `, function (res) {
         if(res === 'yes'){
             callback(true);
-            reader.close();
+            // reader.close();
 
         } else {
             callback(false);
-            reader.close();
+            // reader.close();
         }
     })
 
@@ -43,10 +39,25 @@ function innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop) {
                 arr[i] = arr[i+1];
                 arr[i+1] = holder;
                 madeAnySwaps = true;
+                console.log(arr);
             }
             innerBubbleSortLoop(arr, i+1, madeAnySwaps, outerBubbleSortLoop);
         })
     }
-
-
 }
+
+function absurdBubbleSort(arr, sortCompletionCallback){
+    function outerBubbleSortLoop(madeAnySwaps){
+        if (madeAnySwaps){
+            innerBubbleSortLoop(arr, 0, false, outerBubbleSortLoop)
+        }else{
+            sortCompletionCallback(arr)
+        }
+    }
+    outerBubbleSortLoop(true)
+}
+
+absurdBubbleSort([3, 2, 1], function (arr) {
+    console.log("Sorted array: " + JSON.stringify(arr));
+    reader.close();
+});
