42d8814b3e776b8339b48a2d8a1400ce7aeca2d4
Michelle Ha <michelle1992us@yahoo.com>
2021-01-15 14:48:17 -0800

    finished innerbubblesortloop

--- w8d5/bubblesort.js
+++ w8d5/bubblesort.js
@@ -12,11 +12,41 @@ function absurdBubbleSort(){
 
 function askIfGreaterThan(el1, el2, callback) {
     reader.question(`Is ${el1} greater than ${el2}? `, function (res) {
-        if(response === 'yes'){
-            callback(true)
+        if(res === 'yes'){
+            callback(true);
+            reader.close();
+
+        } else {
+            callback(false);
+            reader.close();
         }
     })
 
     // Prompt user to tell us whether el1 > el2; pass true back to the
     // callback if true; else false.
+}
+
+// askIfGreaterThan(7, 5, function (res) {
+//     console.log(res)
+// })
+
+function innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop) {
+    if (madeAnySwaps === undefined) {
+        madeAnySwaps = false;
+    }
+    if (i === arr.length - 1) {
+        outerBubbleSortLoop(madeAnySwaps);
+    } else {
+        askIfGreaterThan(arr[i], arr[i+1], function (isGreaterThan) {
+            if (isGreaterThan) {
+                let holder = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = holder;
+                madeAnySwaps = true;
+            }
+            innerBubbleSortLoop(arr, i+1, madeAnySwaps, outerBubbleSortLoop);
+        })
+    }
+
+
 }
