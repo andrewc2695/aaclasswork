7c5bca85623bd08e7c301450df315afda078ad7a
Andrew Curd <andrewc2695@gmail.com>
2021-01-15 16:37:48 -0800

    wrote my throttle

--- w8d5/myThrottle.js
+++ w8d5/myThrottle.js
@@ -6,12 +6,83 @@
 
 Function.prototype.myThrottle = function(time) {
     let tooSoon = false;
-    if (!tooSoon) {
-        tooSoon = true;
-        setTimeout(() => tooSoon = false, time);
-        this();
+    let that = this
+    return function () {
+        if (!tooSoon) {
+            tooSoon = true;
+            setTimeout(() => tooSoon = false, time);
+            that();
+        }
     }
 }
 
 
-  
+// class Neuron {
+//     fire() {
+//         console.log("Firing!");
+//     }
+// }
+
+// const neuron = new Neuron();
+// // When we create a new Neuron,
+// // we can call #fire as frequently as we want
+
+// // The following code will try to #fire the neuron every 10ms. Try it in the console:
+
+// neuron.fire = neuron.fire.myThrottle(500);
+
+
+// const interval = setInterval(() => {
+//     neuron.fire();
+// }, 10);
+
+class SearchBar {
+    constructor() {
+        this.query = "";
+
+        this.type = this.type.bind(this);
+        this.search = this.search.bind(this);
+    }
+
+    type(letter) {
+        this.query += letter;
+        this.search();
+    }
+
+    search() {
+        console.log(`searching for ${this.query}`);
+    }
+}
+
+
+const searchBar = new SearchBar();
+
+const queryForHelloWorld = () => {
+    searchBar.type("h");
+    searchBar.type("e");
+    searchBar.type("l");
+    searchBar.type("l");
+    searchBar.type("o");
+    searchBar.type(" ");
+    searchBar.type("w");
+    searchBar.type("o");
+    searchBar.type("r");
+    searchBar.type("l");
+    searchBar.type("d");
+};
+
+
+
+
+Function.prototype.myDebounce = function(time){
+    let timeout = setTimeout(() => that(), time)
+    let that = this
+    return function () {
+       clearTimeout(timeout)
+       timeout = setTimeout(() => that(), time)
+    }
+}
+
+searchBar.search = searchBar.search.myDebounce(500);
+
+queryForHelloWorld();
