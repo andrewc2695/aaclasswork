cf1b924c5eeeaaa8a3bac7bc313c6ef1b7275f69
Andrew Curd <andrewc2695@gmail.com>
2021-01-20 11:39:11 -0800

    started jquery tic tac toe

--- w9d3/jqTTT/src/index.js
+++ w9d3/jqTTT/src/index.js
@@ -1,6 +1,8 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const View = require('./ttt-view');// require appropriate file
+const Game = require('./game');// require appropriate file
 
   $(() => {
-    // Your code here
+    const game = new Game();
+    const el = $('.ttt');
+    const view = new View(game, el);
   });
--- w9d3/jqTTT/src/ttt-view.js
+++ w9d3/jqTTT/src/ttt-view.js
@@ -5,7 +5,17 @@ class View {
 
   makeMove($square) {}
 
-  setupBoard() {}
+  setupBoard() {
+    const $ul = $('<ul>');
+    for(let i = 0; i < 3; i++){
+      for(let j = 0; j < 3; j++){
+        let $li = $('<li>');
+      }
+    }
+  }
+
 }
+
+
 
 module.exports = View;
