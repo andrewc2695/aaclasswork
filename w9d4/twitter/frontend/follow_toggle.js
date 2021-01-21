4d3a32d9b8015edf3eb26bc70317814712a2cbdf
Qi Zhu <rubybfm1017@gmail.com>
2021-01-21 11:53:48 -0800

    twitter.js

--- w9d4/twitter/frontend/follow_toggle.js
+++ w9d4/twitter/frontend/follow_toggle.js
@@ -1,3 +1,18 @@
 class FollowToggle{
-    constructor(el)
+    constructor(el){
+        this.$el = $(el);
+        this.user_id = this.$el.data('user_id');
+        this.followState = this.$el.data('initial - follow - state');
+        this.render();
+    }
+
+    render(){
+        if (this.followState){
+            this.$el.html('Unfollow');
+        }else{
+            this.$el.html('Follow');
+        }
+    }
 }
+
+module.exports = FollowToggle;
