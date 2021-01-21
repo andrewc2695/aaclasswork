e894679e3493c99b5e0883a97a9838f025009d03
Andrew Curd <andrewc2695@gmail.com>
2021-01-21 13:40:52 -0800

    started ajax

--- w9d4/twitter/app/assets/javascripts/bundle.js
+++ w9d4/twitter/app/assets/javascripts/bundle.js
@@ -1,7 +1,93 @@
 /******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./frontend/follow_toggle.js":
+/*!***********************************!*\
+  !*** ./frontend/follow_toggle.js ***!
+  \***********************************/
+/***/ ((module) => {
+
+class FollowToggle{
+    constructor(el){
+        this.$el = $(el);
+        this.user_id = this.$el.data('user_id');
+        this.followState = this.$el.data('initial-follow-state');
+        this.render();
+        this.$el.on('click', this.handleClick())
+    }
+
+    render(){
+        if (this.followState){
+            this.$el.html('Unfollow');
+        }else{
+            this.$el.html('Follow');
+        }
+    }
+
+    handleClick(e){
+        e.preventDefault
+        if(this.followState){
+            this.followState = false;
+            $.ajax({
+                method: 'DELETE',
+                url: '/users/:user_id/follow',
+                data: this.followState,
+                dataType: 'JSON'
+            })
+        }else{
+            this.followState = true;
+            $.ajax({
+                method: 'POST',
+                url: '/users/:user_id/follow',
+                data: this.followState,
+                dataType: 'JSON'
+            })
+        }
+    }
+}
+
+module.exports = FollowToggle;
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		if(__webpack_module_cache__[moduleId]) {
+/******/ 			return __webpack_module_cache__[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+(() => {
 /*!*****************************!*\
   !*** ./frontend/twitter.js ***!
   \*****************************/
+let FT = __webpack_require__(/*! ./follow_toggle */ "./frontend/follow_toggle.js");
+
+$(function(){
+    $('button.follow-toggle').each ((i, button) => new FT(button));
+});
+
+})();
 
 /******/ })()
 ;
+//# sourceMappingURL=bundle.js.map
--- w9d4/twitter/frontend/follow_toggle.js
+++ w9d4/twitter/frontend/follow_toggle.js
@@ -2,8 +2,9 @@ class FollowToggle{
     constructor(el){
         this.$el = $(el);
         this.user_id = this.$el.data('user_id');
-        this.followState = this.$el.data('initial - follow - state');
+        this.followState = this.$el.data('initial-follow-state');
         this.render();
+        this.$el.on('click', this.handleClick())
     }
 
     render(){
@@ -11,6 +12,27 @@ class FollowToggle{
             this.$el.html('Unfollow');
         }else{
             this.$el.html('Follow');
+        }
+    }
+
+    handleClick(e){
+        e.preventDefault
+        if(this.followState){
+            this.followState = false;
+            $.ajax({
+                method: 'DELETE',
+                url: '/users/:user_id/follow',
+                data: this.followState,
+                dataType: 'JSON'
+            })
+        }else{
+            this.followState = true;
+            $.ajax({
+                method: 'POST',
+                url: '/users/:user_id/follow',
+                data: this.followState,
+                dataType: 'JSON'
+            })
         }
     }
 }
