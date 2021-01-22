89a99c374bd66114e99a40243bed9ac244e7241d
IsoVoyd <dubnersamuel@gmail.com>
2021-01-22 15:28:27 -0800

    work on todo_list

--- w9d5/pocketprojects/src/todo_list.js
+++ w9d5/pocketprojects/src/todo_list.js
@@ -1,2 +1,18 @@
+const todoList = [];
+const ulTodos = document.querySelector(".todos")
+const todoForm = document.querySelector(".add-todo-form")
 
+function addTodo(element) {
+    element.preventDefault();
+    let todo = document.querySelector("input[name=add-todo]")
+    let todoObj = {
+        value: todo.value,
+        done: false
+    }
+    todoList.push(todoObj);
+    todo.value = "";
+}
 
+function populateList() {
+    
+}
